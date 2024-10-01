b20fb3f0689f590c42ce1ea107196b5705e8459b
nirmal <nirmal@nirmal.com>
2024-10-01 16:17:50 +0530

    CommandLineRunner code added, basic implementation

--- src/main/java/com/te/ems/EmployeeManagementSystemApplication.java
+++ src/main/java/com/te/ems/EmployeeManagementSystemApplication.java
@@ -1,13 +1,22 @@
 package com.te.ems;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EmployeeManagementSystemApplication {
 
 	public static void main(String[] args) {
+		System.out.println("EMSApplication");
 		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
 	}
 
+@Bean
+public CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println("commandLineRunner() method executed!");
+		};
+}
 }
